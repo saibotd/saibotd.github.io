12a187410d5f2bee9b4d024864380bd84644d314
Tobias Duehr <tobias.duehr@b2online.de>
2013-07-17 10:34:03 +0200

    Initial

---
+++ js/index.js
@@ -0,0 +1,49 @@
+(function () {
+    var canvas = document.getElementById('stage'),
+        c = canvas.getContext('2d');
+    window.addEventListener('resize', resizeCanvas, false);
+
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+    resizeCanvas();
+
+    window.addEventListener('mousemove', drawStuff, false);
+    document.addEventListener('touchmove', drawStuff, false);
+
+    function drawStuff(e) {
+
+        x = 500;
+        y = 0;
+      offset_x = 0;
+
+        if (e && e.type === "mousemove") {
+            x = e.clientX;
+            y = e.clientY;
+        }
+
+        if (e && e.type === "touchmove") {
+            var touch = e.touches[0];
+            x = touch.clientX;
+            y = touch.clientY;
+        }
+
+        c.fillStyle = 'rgba(163,26,11,.25)';
+        c.beginPath();
+        c.moveTo(0, canvas.height);
+        c.lineTo(x, y);
+        c.lineTo(canvas.width / 2 + offset_x, canvas.height);
+        c.closePath();
+        c.fill();
+
+        c.fillStyle = 'rgba(11,148,163,.25)';
+        c.beginPath();
+        c.moveTo(canvas.width, canvas.height);
+        c.lineTo(x, y);
+        c.lineTo(canvas.width / 2 + offset_x, canvas.height);
+        c.closePath();
+        c.fill();
+
+    }
+})();
