f9601e411588f8e4126364b52016f5c32fbc6e1f
Tobias Duehr <tobias.duehr@b2online.de>
2013-07-18 13:36:15 +0200

    Cleaned up the JS a bit

--- js/index.js
+++ js/index.js
@@ -1,57 +1,11 @@
-(function () {
-    var sideBarVisible = true;
-    var canvas = document.getElementById('stage'),
-    c = canvas.getContext('2d');
-    window.addEventListener('resize', resizeCanvas, false);
+var sideBarVisible = true;
 
-    function resizeCanvas() {
+function initBackgroundFX(){
+    resizeCanvas = function() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-    }
-    resizeCanvas();
-
-    function showSidebar() {
-        if(!sideBarVisible){
-            $('.sidebar').transition({ right: '0px' });
-            sideBarVisible = true;
-            $("#toggle-sidebar").attr("class", "visible");
-        }
-    }
-
-    function hideSidebar() {
-        if(sideBarVisible){
-            $('.sidebar').transition({ right: '-640px' });
-            sideBarVisible = false;
-            $("#toggle-sidebar").attr("class", "hidden");
-        }
-    }
-
-    $(".content").click(function(e){
-        if(e.target.id != "more" && document.width - e.pageX > 640){
-            hideSidebar();
-        }
-    });
-
-    $("#toggle-sidebar").click(function(e){
-        e.preventDefault();
-        if(sideBarVisible) hideSidebar();
-        else showSidebar();
-    });
-
-    window.addEventListener('mousemove', drawStuff, false);
-    document.addEventListener('touchmove', drawStuff, false);
-
-    var hammertime = $("body").hammer({
-        stop_browser_behavior: false,
-        prevent_mouseevents: true
-    });
-
-    hammertime.on("swipeleft", showSidebar);
-
-    hammertime.on("swiperight", hideSidebar);
-
-    function drawStuff(e) {
-
+    };
+    drawStuff = function(e) {
         x = 500;
         y = 0;
         offset_x = 0;
@@ -82,27 +36,90 @@
         c.lineTo(canvas.width / 2 + offset_x, canvas.height);
         c.closePath();
         c.fill();
-
+    };
+    if(!!window.CanvasRenderingContext2D){
+        var canvas = document.getElementById('stage'),
+        c = canvas.getContext('2d');
+        window.addEventListener('resize', resizeCanvas, false);
+        resizeCanvas();
+        window.addEventListener('mousemove', drawStuff, false);
+        document.addEventListener('touchmove', drawStuff, false);
     }
+}
 
+function initSidebar(){
+    var $sidebar = $('.sidebar');
+    var $content = $('.content');
+    var $toggleSidebar = $('#toggle-sidebar');
+    showSidebar = function() {
+        if(!sideBarVisible){
+            $sidebar.transition({ right: '0px' });
+            sideBarVisible = true;
+            $toggleSidebar.attr("class", "visible");
+        }
+    };
+
+    hideSidebar = function() {
+        if(sideBarVisible){
+            $sidebar.transition({ right: '-640px' });
+            sideBarVisible = false;
+            $toggleSidebar.attr("class", "hidden");
+        }
+    };
+
+    $content.click(function(e){
+        if(e.target.id != "more" && document.width - e.pageX > 640){
+            hideSidebar();
+        }
+    });
+
+    $toggleSidebar.click(function(e){
+        e.preventDefault();
+        if(sideBarVisible) hideSidebar();
+        else showSidebar();
+    });
+}
+
+function initNavigation(){
+    var $sidebar = $('.sidebar');
     $(".topbar > nav > a").click(function(e){
         e.preventDefault();
         var href = this.href;
         if(!sideBarVisible) $('.sidebar-inner').html("");
         showSidebar();
-        $(".sidebar").addClass("loading");
+        $sidebar.addClass("loading");
         $('.sidebar-inner').transition({opacity:0},function(){
-            $(".sidebar").load(href + " .sidebar-inner", function(){
+            $sidebar.load(href + " .sidebar-inner", function(){
                 $('.sidebar-inner').css("opacity", 0);
                 $('.sidebar-inner').transition({opacity:1});
-                $(".sidebar").removeClass("loading");
+                $sidebar.removeClass("loading");
             });
         });
         history.pushState({}, $(this).text(), href);
     });
-
-    if(thisIsIndex){
-        $(".sidebar").load("home.html .sidebar-inner");
+    if(typeof thisIsIndex !== undefined){
+        $sidebar.load("home.html .sidebar-inner");
         hideSidebar();
     }
+}
+
+function initSwipe(){
+    var hammertime = $("body").hammer({
+        stop_browser_behavior: false,
+        prevent_mouseevents: true
+    });
+
+    hammertime.on("swipeleft", showSidebar);
+
+    hammertime.on("swiperight", hideSidebar);
+}
+
+
+(function () {
+
+    initBackgroundFX();
+    initSidebar();
+    initNavigation();
+    initSwipe();
+    
 })();
