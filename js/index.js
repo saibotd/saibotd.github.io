c2376d83667ba9b091f78e7ea5c513c56febe310
Tobias Duehr <tobi@saibotd.com>
2013-08-01 09:53:08 +0200

    dunno

--- js/index.js
+++ js/index.js
@@ -3,6 +3,15 @@ var sideBarWidthNormal = 640;
 var sideBarWidthWide = 840;
 var popState = false;
 
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+Array.prototype.randomElement = function () {
+  return this[Math.floor(Math.random() * this.length)];
+}
+
+
 function initBackgroundFX2D(){
     resizeCanvas = function() {
         canvas.width = window.innerWidth;
@@ -23,12 +32,12 @@ function initBackgroundFX2D(){
             x = touch.clientX;
             y = touch.clientY;
         }
-
+        var center = (sideBarVisible) ? (canvas.width - 640) / 2 : canvas.width / 2
         c.fillStyle = 'rgba(163,26,11,.15)';
         c.beginPath();
         c.moveTo(0, canvas.height);
         c.lineTo(x, y);
-        c.lineTo(canvas.width / 2 + offset_x, canvas.height);
+        c.lineTo(center + offset_x, canvas.height);
         c.closePath();
         c.fill();
 
@@ -36,7 +45,7 @@ function initBackgroundFX2D(){
         c.beginPath();
         c.moveTo(canvas.width, canvas.height);
         c.lineTo(x, y);
-        c.lineTo(canvas.width / 2 + offset_x, canvas.height);
+        c.lineTo(center + offset_x, canvas.height);
         c.closePath();
         c.fill();
 
@@ -47,8 +56,133 @@ function initBackgroundFX2D(){
         window.addEventListener('resize', resizeCanvas, false);
         resizeCanvas();
         window.addEventListener('mousemove', drawStuff, false);
-        document.addEventListener('touchmove', drawStuff, false);
+        window.addEventListener('touchmove', drawStuff, false);
     }
+}
+
+function initBackgroundFX2DSubtle(){
+    var canvas = document.getElementById('stage'),
+    c = canvas.getContext('2d'),
+    size,
+    counter = 0,
+    width = 30,
+    mouse_x = -1,
+    mouse_y = -1,
+    fps = 60,
+    init = true,
+    now,
+    then = Date.now(),
+    interval = 1000/fps,
+    matrix = [];
+
+    window.addEventListener('resize', resizeCanvas, false);
+    window.addEventListener('mousemove', trackMouse, false);
+    window.addEventListener('touchmove', trackMouse, false);
+
+    function resizeCanvas() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      size = Math.ceil(canvas.width / width);
+      counter = 0;
+      for(var x = 0; x < width * 2; x++){
+        for(var y = 0; y < Math.ceil(canvas.height / size); y++){
+          matrix.push({
+            x : x,
+            y : y,
+            _x : x * size / 2,
+            _y : y * size
+        });
+      }
+  }
+  init = true;
+  draw();
+}
+resizeCanvas();
+
+function trackMouse(e){
+  mouse_x = -1;
+  mouse_y = -1;
+  
+  if (e && e.type === "mousemove") {
+    mouse_x = e.clientX;
+    mouse_y = e.clientY;
+}
+
+if (e && e.type === "touchmove") {
+    var touch = e.touches[0];
+    mouse_x = touch.clientX;
+    mouse_y = touch.clientY;
+}
+
+for(var i in matrix){
+  var tile = matrix[i];
+
+  if(
+    Math.random() < .2 &&
+    tile._x <= mouse_x &&
+    tile._x + size > mouse_x &&
+    tile._y <= mouse_y &&
+    tile._y + size > mouse_y
+    ){
+
+      _color = getRandomInt(110,130);
+  color = [_color,_color,_color];
+
+  c.fillStyle = "rgba("+color+",.1)";
+  c.beginPath();
+
+  if(tile.x % 2 == 0){
+    c.moveTo(tile._x - size / 2, tile._y);
+    c.lineTo(tile._x + size / 2, tile._y);
+    c.lineTo(tile._x, tile._y + size);
+} else {
+    c.moveTo(tile._x - size/2, tile._y + size);
+    c.lineTo(tile._x, tile._y);
+    c.lineTo(tile._x + size/2, tile._y + size);
+}
+c.closePath();
+c.fill();
+}
+}
+
+}
+
+function draw(){
+  init = false;
+  var counter = 12;
+  if(init) counter = matrix.length;
+  console.log();
+  now = Date.now();
+  delta = now - then;
+  if (delta > interval) {
+    while(counter > 0){
+      counter = counter - 1;
+      var tile = matrix.randomElement();
+      
+      _color = getRandomInt(160,200);
+      color = [_color,_color,_color];
+      c.fillStyle = "rgba("+color+",.1)";
+      
+      c.beginPath();
+      
+      if(tile.x % 2 == 0){
+        c.moveTo(tile._x - size / 2, tile._y);
+        c.lineTo(tile._x + size / 2, tile._y);
+        c.lineTo(tile._x, tile._y + size);
+    } else {
+        c.moveTo(tile._x - size/2, tile._y + size);
+        c.lineTo(tile._x, tile._y);
+        c.lineTo(tile._x + size/2, tile._y + size);
+    }
+    c.closePath();
+    c.fill();
+}
+
+then = now - (delta % interval);
+}
+window.requestAnimationFrame(draw);
+}
+
 }
 
 function initBackgroundFX3D(){
@@ -173,112 +307,112 @@ function initBackgroundFX3D(){
             }
 
             function render() {
-               camera.position.x += ( mouseX - camera.position.x ) * .0080;
-                camera.position.y += ( - mouseY - camera.position.y ) * .0080;
+             camera.position.x += ( mouseX - camera.position.x ) * .0080;
+             camera.position.y += ( - mouseY - camera.position.y ) * .0080;
 
-                camera.lookAt( scene.position );
+             camera.lookAt( scene.position );
 
-                var currentSeconds = Date.now();
-                group.rotation.x = Math.sin( currentSeconds * 0.0007 ) * 0.5;
-                group.rotation.y = Math.sin( currentSeconds * 0.0003 ) * 0.5;
-                group.rotation.z = Math.sin( currentSeconds * 0.0002 ) * 0.5;
+             var currentSeconds = Date.now();
+             group.rotation.x = Math.sin( currentSeconds * 0.0007 ) * 0.5;
+             group.rotation.y = Math.sin( currentSeconds * 0.0003 ) * 0.5;
+             group.rotation.z = Math.sin( currentSeconds * 0.0002 ) * 0.5;
 
-                renderer.render( scene, camera );
+             renderer.render( scene, camera );
 
-            } 
-        }
+         } 
+     }
 
-        function initSidebar(){
-            var $sidebar = $('.sidebar');
-            var $content = $('.content');
-            var $toggleSidebar = $('#toggle-sidebar');
-            showSidebar = function() {
-                if(!sideBarVisible){
-                    $sidebar.removeClass("hidden");
-                    $toggleSidebar.removeClass("hidden");
-                    sideBarVisible = true;
-                }
-            };
-
-            hideSidebar = function() {
-                var sideBarWidth = ($('body').hasClass("wide") ? sideBarWidthWide : sideBarWidthNormal);
-                if(sideBarVisible){
-                    $sidebar.addClass("hidden");
-                    $toggleSidebar.addClass("hidden");
-                    sideBarVisible = false;
-                }
-            };
-
-            $content.click(function(e){
-                var sideBarWidth = ($('body').hasClass("wide") ? sideBarWidthWide : sideBarWidthNormal);
-                if(e.target.id != "more" && document.width - e.pageX > sideBarWidth){
-                    hideSidebar();
-                }
-            });
-
-            $toggleSidebar.click(function(e){
-                e.preventDefault();
-                if(sideBarVisible) hideSidebar();
-                else showSidebar();
-            });
-        }
-
-        function navigate(href){
-            var $sidebar = $('.sidebar');
-            if(!sideBarVisible) $('.sidebar-inner').html("");
-            showSidebar();
-            $sidebar.addClass("loading");
-            $('.sidebar-inner').transition({opacity:0},function(){
-                $sidebar.load(href + " .sidebar-inner", function(){
-                    $('.sidebar-inner').css("opacity", 0);
-                    $('.sidebar-inner').transition({opacity:1});
-                    $sidebar.removeClass("loading");
-                    $(".work > li > a").ajaxify();
-                });
-            });
-        }
-
-        $.fn.ajaxify = function() {
-            $(this).click(function(e){
-                e.preventDefault();
-                navigate(this.href);
-                if(!!history.pushState) history.pushState(null, null, this.href);
-                $("body").removeClass("wide");
-                if($(this).hasClass("wide")) $("body").addClass("wide");
-            });
+     function initSidebar(){
+        var $sidebar = $('.sidebar');
+        var $content = $('.content');
+        var $toggleSidebar = $('#toggle-sidebar');
+        showSidebar = function() {
+            if(!sideBarVisible){
+                $sidebar.removeClass("hidden");
+                $toggleSidebar.removeClass("hidden");
+                sideBarVisible = true;
+            }
         };
 
-        function initNavigation(){
-            $(".topbar > .nav a, .work > li > a").ajaxify();
-
-            if(window.location.pathname === "/") hideSidebar();
-
-            if ("popState" in window || "popstate" in window) {
-                window.addEventListener('popstate', function(e) {
-                    if(popState) navigate(window.location.pathname);
-                    else popState = true;
-                });
+        hideSidebar = function() {
+            var sideBarWidth = ($('body').hasClass("wide") ? sideBarWidthWide : sideBarWidthNormal);
+            if(sideBarVisible){
+                $sidebar.addClass("hidden");
+                $toggleSidebar.addClass("hidden");
+                sideBarVisible = false;
             }
+        };
 
-        }
+        $content.click(function(e){
+            var sideBarWidth = ($('body').hasClass("wide") ? sideBarWidthWide : sideBarWidthNormal);
+            if(e.target.id != "more" && document.width - e.pageX > sideBarWidth){
+                hideSidebar();
+            }
+        });
 
-        function initSwipe(){
-            var hammertime = $("body").hammer({
-                stop_browser_behavior: false,
-                prevent_mouseevents: true
+        $toggleSidebar.click(function(e){
+            e.preventDefault();
+            if(sideBarVisible) hideSidebar();
+            else showSidebar();
+        });
+    }
+
+    function navigate(href){
+        var $sidebar = $('.sidebar');
+        if(!sideBarVisible) $('.sidebar-inner').html("");
+        showSidebar();
+        $sidebar.addClass("loading");
+        $('.sidebar-inner').transition({opacity:0},function(){
+            $sidebar.load(href + " .sidebar-inner", function(){
+                $('.sidebar-inner').css("opacity", 0);
+                $('.sidebar-inner').transition({opacity:1});
+                $sidebar.removeClass("loading");
+                $(".work > li > a").ajaxify();
             });
+        });
+    }
 
-            hammertime.on("swipeleft", showSidebar);
+    $.fn.ajaxify = function() {
+        $(this).click(function(e){
+            e.preventDefault();
+            navigate(this.href);
+            if(!!history.pushState) history.pushState(null, null, this.href);
+            $("body").removeClass("wide");
+            if($(this).hasClass("wide")) $("body").addClass("wide");
+        });
+    };
 
-            hammertime.on("swiperight", hideSidebar);
+    function initNavigation(){
+        $(".topbar > .nav a, .work > li > a").ajaxify();
+
+        if(window.location.pathname === "/") hideSidebar();
+
+        if ("popState" in window || "popstate" in window) {
+            window.addEventListener('popstate', function(e) {
+                if(popState) navigate(window.location.pathname);
+                else popState = true;
+            });
         }
 
+    }
 
-        (function () {
-            if ((Math.random() < 0.2  || window.location.hash == "#3d") && window.WebGLRenderingContext) initBackgroundFX3D();
-            else initBackgroundFX2D();
-            initSidebar();
-            initNavigation();
-            initSwipe();
+    function initSwipe(){
+        var hammertime = $("body").hammer({
+            stop_browser_behavior: false,
+            prevent_mouseevents: true
+        });
 
-        })();
+        hammertime.on("swipeleft", showSidebar);
+
+        hammertime.on("swiperight", hideSidebar);
+    }
+
+
+    (function () {
+        if ((Math.random() < 0.2  || window.location.hash == "#3d") && window.WebGLRenderingContext) initBackgroundFX3D();
+        else initBackgroundFX2D();
+        initSidebar();
+        initNavigation();
+        initSwipe();
+
+    })();
